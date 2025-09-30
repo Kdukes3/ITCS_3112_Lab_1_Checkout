1d2d08c7ece87df0a4ac14543c3de0525623bd5c
Donovan <djohn304@charlotte.edu>
2025-09-30 14:42:11 -0400

    Changed file

--- ITCS_3112_Lab_1_Checkout/Catalog.cs
+++ ITCS_3112_Lab_1_Checkout/Catalog.cs
@@ -2,5 +2,23 @@ namespace ITCS_3112_Lab_1_Checkout;
 
 public class Catalog : ICatalog
 {
-    
+    public List<Item> ListAvailable()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Item> ListUnavailable()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Item FindById(string itemId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Item> SearchBy(int criteria, string query)
+    {
+        throw new NotImplementedException();
+    }
 }
